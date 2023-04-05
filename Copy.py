8919e71399c416886effad2843053a37f400bb43
Stefan <hai_kazuma_desu@web.de>
2023-04-06 00:56:57 +0200

    Claire, Forbidden banner + Bakugen
    Claire, Forbidden banner + Bakugen

---
+++ Copy.py
@@ -0,0 +1,45 @@
+import shutil
+import os.path
+
+if __name__ == '__main__':
+    current = os.getcwd();
+    parent = os.path.dirname(current)
+    print(parent)
+    source_dir = os.path.join(parent, "konofan-assets-jp-sortet", "Assets", "AddressableAssetsStore", "UnityAssets", "Common", "Images")
+    destination_dir = os.path.join(current, "Assets", "AddressableAssetsStore", "UnityAssets", "Common", "Images")
+
+    print(source_dir)
+    print(destination_dir)
+
+    src_dir = os.path.join(source_dir, "IconAccessory")
+    dst_dir = os.path.join(destination_dir, "IconAccessory")
+
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+    src_dir = os.path.join(source_dir, "IconAssist")
+    dst_dir = os.path.join(destination_dir, "IconAssist")
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+    src_dir = os.path.join(source_dir, "IconEnemy")
+    dst_dir = os.path.join(destination_dir, "IconEnemy")
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+    src_dir = os.path.join(source_dir, "IconLargeMember")
+    dst_dir = os.path.join(destination_dir, "IconLargeMember")
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+    src_dir = os.path.join(source_dir, "IconMember")
+    dst_dir = os.path.join(destination_dir, "IconMember")
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+    src_dir = os.path.join(source_dir, "IconMiddleMember")
+    dst_dir = os.path.join(destination_dir, "IconMiddleMember")
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+    src_dir = os.path.join(source_dir, "IconSpattack")
+    dst_dir = os.path.join(destination_dir, "IconSpattack")
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+    src_dir = os.path.join(source_dir, "IconWeapon")
+    dst_dir = os.path.join(destination_dir, "IconWeapon")
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
